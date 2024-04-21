d2f824ed18df415a21593f27db5edf1ddbac0ca5
Davlatjon Shavkatov <opensource@dalisoft.uz>
2024-04-22 00:37:52 +0500

    fix(core): add variant without `GH_TOKEN`

--- bin-dl.sh
+++ bin-dl.sh
@@ -69,11 +69,18 @@ while :; do
   shift
 done
 
-RELEASES=$(curl -sL \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${GH_TOKEN}" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  "https://api.github.com/repos/${repository}/releases")
+if [ -n "${GH_TOKEN-}" ]; then
+  RELEASES=$(curl -sL \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer ${GH_TOKEN-}" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    "https://api.github.com/repos/${repository}/releases")
+else
+  RELEASES=$(curl -sL \
+    -H "Accept: application/vnd.github+json" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    "https://api.github.com/repos/${repository}/releases")
+fi
 DOWNLOAD_URLS=$(echo "${RELEASES}" | grep "browser_download_url")
 
 for os_name in ${OS}; do
